2be68a639243f33262e38a3a0681792d55d28a60
Matt Nowack <matt@discordapp.com>
2021-09-17 18:07:15 -0700

    Even more polish

--- lib/mix/tasks/patch.release.ex
+++ lib/mix/tasks/patch.release.ex
@@ -1,16 +1,30 @@
 defmodule Mix.Tasks.Patch.Release do
   @moduledoc """
   Prepares Patch for the next release.
+
+  It is expected that the version will be updated in mix.exs **before** running
+  this tool.
+
+  This tool will collect some necessary information for the changelog.
+
+  There are some minor differences in how Github and ExDocs want to link and
+  render things.  This task also takes care of generating custom markdown that
+  renders properly in Github and ExDocs.  Github markdown files end up in the
+  root of the project, the ExDocs markdown files end up in /pages and are
+  included via extras in mix.exs.
   """
+  @shortdoc "Prepares Patch for the next release."
 
   use Mix.Task
 
-  @emojis %{
-    improvement: "⬆️",
-    feature: "🎁",
-    bugfix: "🐞",
-    deprecation: "⚠️",
-    removal: "⛔️"
+  @exdocs_links %{
+    assert_refute_calls: "#asserting-refuting-calls",
+    changelog: "changelog.html"
+  }
+
+  @github_links %{
+    assert_refute_calls: "#asserting--refuting-calls",
+    changelog: "CHANGELOG.md"
   }
 
   def run(_) do
@@ -18,7 +32,7 @@ defmodule Mix.Tasks.Patch.Release do
     version = config[:version]
 
     releases =
-      "./templates/releases.etf"
+      "./pages/templates/releases.etf"
       |> Path.expand()
       |> File.read!()
       |> :erlang.binary_to_term()
@@ -65,18 +79,29 @@ defmodule Mix.Tasks.Patch.Release do
   def render_files(version, releases) do
     releases_binary = :erlang.term_to_binary(releases)
 
-    readme_template = Path.expand("./templates/README.eex")
-    changelog_template = Path.expand("./templates/CHANGELOG.eex")
+    readme_template = Path.expand("./pages/templates/README.eex")
+    changelog_template = Path.expand("./pages/templates/CHANGELOG.eex")
+    releases_path = Path.expand("./pages/templates/releases.etf")
 
-    changelog_path = Path.expand("./CHANGELOG.md")
-    readme_path = Path.expand("./README.md")
-    releases_path = Path.expand("./templates/releases.etf")
+    exdocs_changelog_path = Path.expand("./pages/CHANGELOG.md")
+    github_changelog_path = Path.expand("./CHANGELOG.md")
 
-    changelog = EEx.eval_file(changelog_template, [releases: releases, emojis: @emojis])
-    readme = EEx.eval_file(readme_template, [version: version, emojis: @emojis])
+    exdocs_readme_path = Path.expand("./pages/README.md")
+    github_readme_path = Path.expand("./README.md")
 
-    File.write!(changelog_path, changelog)
-    File.write!(readme_path, readme)
+
+    exdocs_changelog = EEx.eval_file(changelog_template, [releases: releases])
+    github_changelog = EEx.eval_file(changelog_template, [releases: releases])
+
+    exdocs_readme = EEx.eval_file(readme_template, [version: version, links: @exdocs_links])
+    github_readme = EEx.eval_file(readme_template, [version: version, links: @github_links])
+
+    File.write!(exdocs_changelog_path, exdocs_changelog)
+    File.write!(github_changelog_path, github_changelog)
+
+    File.write!(exdocs_readme_path, exdocs_readme)
+    File.write!(github_readme_path, github_readme)
+
     File.write!(releases_path, releases_binary)
 
     info(["Version ", :cyan, version, :default_color, " has been ", :green, "successfully", :default_color, " prepared for release. 🚀"])
