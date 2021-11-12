44475d4394259258d8ea919ef6652d17430f0e2e
Matt Nowack <mdnowack@gmail.com>
2021-11-12 10:37:23 -0800

    Release v0.8.1 (#26)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Patch.MixProject do
   def project do
     [
       app: :patch,
-      version: "0.8.0",
+      version: "0.8.1",
       elixir: "~> 1.7",
       erlc_paths: erlc_paths(Mix.env()),
       elixirc_paths: elixirc_paths(Mix.env()),
