878ac4255b0c2b5c98b55eb8f047e8bcb6b152ac
Matt Nowack <mdnowack@gmail.com>
2024-05-02 17:56:08 -0700

    Update README and mix.exs for patch release (#65)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Patch.MixProject do
   def project do
     [
       app: :patch,
-      version: "0.13.0",
+      version: "0.13.1",
       elixir: "~> 1.7",
       erlc_paths: erlc_paths(Mix.env()),
       elixirc_paths: elixirc_paths(Mix.env()),
