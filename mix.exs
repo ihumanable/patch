3b176f13d03b3cc283200c2862a869bcc820bef4
Matt Nowack <mdnowack@gmail.com>
2024-12-27 12:37:26 -0500

    Actually bump the project version too

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Patch.MixProject do
   def project do
     [
       app: :patch,
-      version: "0.14.0",
+      version: "0.15.0",
       elixir: "~> 1.9",
       erlc_paths: erlc_paths(Mix.env()),
       elixirc_paths: elixirc_paths(Mix.env()),
